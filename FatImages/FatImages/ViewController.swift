0b08c794ec0760f1aaa7867a1acf616ba4f0169f
Rohan Aurora <rohanaurora@gmail.com>
2020-12-09 14:55:51 -0800

    Complete GDC+Closures revision.

--- FatImages/FatImages/ViewController.swift
+++ FatImages/FatImages/ViewController.swift
@@ -36,18 +36,11 @@ class ViewController: UIViewController {
     // this method downloads a huge image, blocking the main queue and the UI
     // (for instructional purposes only, never do this in a production app)
     @IBAction func synchronousDownload(sender: UIBarButtonItem) {
-        // start animation
         activityView.startAnimating()
-                
-        // use url to get the data for the image
-        if let url = URL(string: BigImages.seaLion.rawValue), let imgData = try? Data(contentsOf: url) {
-            // turn data into an image
+        if let url = URL(string: BigImages.seaLion.rawValue),
+            let imgData = try? Data(contentsOf: url) {
             let image = UIImage(data: imgData)
-            
-            // display it
             self.photoView.image = image
-            
-            // stop animating
             self.activityView.stopAnimating()
         }
     }
@@ -56,14 +49,48 @@ class ViewController: UIViewController {
     
     // this method avoids blocking by creating a background queue, without blocking the UI
     @IBAction func simpleAsynchronousDownload(sender: UIBarButtonItem) {
-        // TODO: finish implementation
-        print("TODO: simpleAsynchronousDownload")
+        
+        activityView.startAnimating()
+        let downloadQueue = DispatchQueue(label: "downloadQ", attributes: [])
+        
+        downloadQueue.async {
+            if let url = URL(string: BigImages.shark.rawValue),
+                let imgData = try? Data(contentsOf: url) {
+                let image = UIImage(data: imgData)
+                
+                DispatchQueue.main.async {
+                    self.photoView.image = image
+                    self.activityView.stopAnimating()
+                }
+            }
+        }
     }
     
     // MARK: - Async Download (with Completion Handler)
     
     @IBAction func asynchronousDownload(sender: UIBarButtonItem) {
-        // TODO: finish implementation
-        print("TODO: asynchronousDownload")
+        
+        activityView.startAnimating()
+        backgroundImageHandler { (img) in
+            DispatchQueue.main.async {
+                self.photoView.image = img
+                self.activityView.stopAnimating()
+            }
+        }
+    }
+    
+    private func backgroundImageHandler(completion handler: @escaping (UIImage) -> Void) {
+        
+        DispatchQueue.global(qos: .userInitiated).async { () -> Void in
+            
+            if let url = URL(string: BigImages.whale.rawValue),
+                let imgData = try? Data(contentsOf: url),
+                let img = UIImage(data: imgData) {
+                
+                DispatchQueue.main.async {
+                    handler(img)
+                }
+            }
+        }
     }
 }
